80c44d66af8e0f6013f0d751cfaeec24ed8882d5
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-04-19 18:27:18 +0800

    Updated utilities to match standard names

--- model_utils/dataloaders.py
+++ model_utils/dataloaders.py
@@ -2,14 +2,11 @@ import torch
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
 from os.path import expanduser
-
-image_size = 64
-batch_size = 128
-workers = 2
+from model_utils.opt import img_size, batch_size, n_cpu
 
 transform = transforms.Compose([
-                transforms.Resize(image_size),
-                transforms.CenterCrop(image_size),
+                transforms.Resize(img_size),
+                transforms.CenterCrop(img_size),
                 transforms.ToTensor(),
                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
@@ -17,11 +14,11 @@ home = expanduser('~')
 data_root = f'{home}/Datasets/cifar-10'
 train_set = dset.CIFAR10(root=data_root, transform=transform)
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
-                                         shuffle=True, num_workers=workers)
+                                         shuffle=True, num_workers=n_cpu)
 test_set = dset.CIFAR10(root=data_root, train=False,
                                        transform=transform)
 test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size,
-                                         shuffle=False, num_workers=workers)
+                                         shuffle=False, num_workers=n_cpu)
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
--- model_utils/opt.py
+++ model_utils/opt.py
@@ -1,12 +1,12 @@
 # Spatial size of training images. All images will be resized to this
 #   size using a transformer.
-image_size = 64
+img_size = 64
 
 # Number of channels in the training images. For color images this is 3
-nc = 3
+channels = 3
 
 # Size of z latent vector (i.e. size of generator input)
-nz = 100
+latent_dim = 100
 
 # Size of feature maps in generator
 ngf = 64
@@ -15,13 +15,32 @@ ngf = 64
 ndf = 64
 
 # Number of training epochs
-num_epochs = 5
+n_epochs = 5
+
+batch_size = 256
 
 # Learning rate for optimizers
-lr = [0.0002, 0.002, 0.02, 0.1]
+lr = 0.0002
 
-# Beta1 hyperparam for Adam optimizers
+# Beta hyperparam for Adam optimizers
 beta1 = 0.5
+beta2 = 0.999
 
 # Number of GPUs available. Use 0 for CPU mode.
 ngpu = 1
+
+n_cpu = 2
+
+n_critic = 5
+
+clip_value = 0.01
+
+sample_interval = 400
+
+# Hyperparameters
+hyperparams = {'lr' : [0.0002, 0.002, 0.02, 0.1],
+               'n_critic' : [5, 10, 15]}
+
+#{'D' : [0.0002, 0.002, 0.02, 0.1],
+#                       'G' : [0.0002, 0.002, 0.02, 0.1]},
+
