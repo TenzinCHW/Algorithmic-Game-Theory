2d70a073ab6741c8ba18f0f7eeb5ca90f8a0f37b
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-04-19 19:03:49 +0800

    Moved get_feat_channels function to DCGAN file

--- model_utils/torchnn.py
+++ model_utils/torchnn.py
@@ -10,11 +10,3 @@ import torchvision.transforms as transforms
 import torchvision.utils as vutils
 import numpy as np
 
-def get_feat_channels(target_feat_channels):
-    feat_channels = []
-    start_c, end_c = target_feat_channels
-    while start_c <= end_c / 2:
-        feat_channels.append(int(start_c))
-        start_c *= 2
-    return feat_channels
-
