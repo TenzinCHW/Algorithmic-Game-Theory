0e85773debe4aa233d6aef759474a177605e6db4
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-04-19 19:10:01 +0800

    Added cudnn.benchmark = True to speed up constant sized inputs

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from Trainer import Trainer
 from models import wgan, wgan_gp, DCGAN, SAGAN
 from model_utils import opt
 from model_utils.dataloaders import train_loader
+from model_utils.torchnn import cudnn
 
+cudnn.benchmark = True
 trainer = Trainer(DCGAN, train_loader, opt)
 trainer.train_hyperparams()
 
