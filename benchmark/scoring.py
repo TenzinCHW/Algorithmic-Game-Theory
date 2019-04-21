f536387b548c38841e3a99d0929e6b5fc5e89f04
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-04-21 22:36:34 +0800

    Modified to loop over all runs for a single model type

--- benchmark/scoring.py
+++ benchmark/scoring.py
@@ -1,32 +1,56 @@
 import os
-from os.path import join
+from os.path import join, expanduser
 import numpy as np
-from fid.fid_score import _compute_statistics_of_path
+import json
+from benchmark.fid.fid_score import _compute_statistics_of_path, calculate_fid_given_paths
+from benchmark.fid.inception import InceptionV3
 
 BATCH_SIZE = 256
 CUDA = True
 DIMS = 2048
 
-def save_ref_statistics():
-    save_test_statistics(expanduser('~/Datasets/cifar-10'), 'results/ref/ref.npz')
+def save_ref_statistics(ref_images_path, ref_stats_dir):
+    os.makedirs(ref_stats_dir, exist_ok=True)
+    inception_model = init_inception()
+    save_statistics(inception_model,
+                    ref_images_path,
+                    'results/ref/ref_fid.npz')
 
-def save_test_statistics(test_path, epoch):
-    save_statistics(join(test_path, 'images', f'{epoch}'), join(test_path, 'bench', f'{epoch}.npz'))
+def save_test_statistics(model_dir):
+    inception_model = init_inception()
+    for run in os.listdir(model_dir):
+        run_dir = join(model_dir, run)
+        images_dir = join(run_dir, 'images')
+        bench_dir = join(run_dir, 'bench')
+        os.makedirs(bench_dir, exist_ok=True)
+        for iteration in os.listdir(images_dir):
+            save_statistics(inception_model,
+                            join(images_dir,
+                                 f'{iteration}'),
+                            join(bench_dir,
+                                 f'{iteration}.npz'))
 
-def save_statistics(path, save_path):
+def init_inception():
     block_idx = InceptionV3.BLOCK_INDEX_BY_DIM[DIMS]
-
     model = InceptionV3([block_idx])
-    if cuda:
+    if CUDA:
         model.cuda()
+    return model.cuda()
 
-    mu, sigma = _compute_statistics_of_path(path, model, BATCH_SIZE, DIMS, CUDA)
+def save_statistics(model, images_dir, save_path):
+    mu, sigma = _compute_statistics_of_path(images_dir, model, BATCH_SIZE, DIMS, CUDA)
     np.savez(save_path, mu=mu, sigma=sigma)
 
-def save_fid_scores(ref_stat_path, test_path):
-    stat_path = join(test_path, 'bench')
-    for path in os.listdir(stat_path):
-        paths = (ref_stat_path, join(stat_path, path))
-        _, _, fid_score = calculate_fid_given_paths(paths, BATCH_SIZE, CUDA, DIMS)
-        np.savez(join(stat_path, f'fid{path}.npz'), )
+def save_fid_scores(ref_stats_path, model_dir):
+    fid_scores = {}
+    for run in os.listdir(model_dir):
+        test_stat_dir = join(model_dir, run, 'bench')
+        for path in os.listdir(test_stat_dir):
+            if 'npz' not in path:
+                continue
+            paths = (ref_stats_path, join(test_stat_dir, path))
+            _, _, fid_score = calculate_fid_given_paths(paths, BATCH_SIZE, CUDA, DIMS)
+            fid_scores[path] = fid_score
+        with open(join(test_stat_dir, 'fid.json'), 'w') as f:
+            json.dump(fid_scores, f, indent=2)
 
