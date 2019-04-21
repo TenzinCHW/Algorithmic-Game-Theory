cc4d35d429c1623cc275a1d9b89ce739119157fc
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-04-21 22:35:37 +0800

    Main training code

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ from model_utils.dataloaders import train_loader
 from model_utils.torchnn import cudnn
 
 cudnn.benchmark = True
+models = [wgan, wgan_gp, DCGAN]
+
 trainer = Trainer(DCGAN, train_loader, opt)
 trainer.train_hyperparams()
+
+for model in models:
+    trainer = Trainer(model, train_loader, opt)
+    trainer.train_hyperparams()
 
