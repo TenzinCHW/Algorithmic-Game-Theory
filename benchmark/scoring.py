a17e4516a9f918e92dc4b958f3f58b99a0c3da59
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-04-22 16:01:34 +0800

    Added inception score

--- benchmark/scoring.py
+++ benchmark/scoring.py
@@ -4,6 +4,7 @@ import numpy as np
 import json
 from benchmark.fid.fid_score import _compute_statistics_of_path, calculate_fid_given_paths
 from benchmark.fid.inception import InceptionV3
+from benchmark.inception.inception_score import inception_score
 
 BATCH_SIZE = 256
 CUDA = True
@@ -18,23 +19,20 @@ def save_ref_statistics(ref_images_path, ref_stats_dir):
 
 def save_test_statistics(model_dir):
     inception_model = init_inception()
-    for run in os.listdir(model_dir):
-        run_dir = join(model_dir, run)
+    for run_dir in loop_path(model_dir):
         images_dir = join(run_dir, 'images')
         bench_dir = join(run_dir, 'bench')
         os.makedirs(bench_dir, exist_ok=True)
-        for iteration in os.listdir(images_dir):
-            save_statistics(inception_model,
-                            join(images_dir,
-                                 f'{iteration}'),
-                            join(bench_dir,
-                                 f'{iteration}.npz'))
+        for iteration in loop_path(images_dir):
+            save_statistics(inception_model, iteration,
+                            join(bench_dir, f'{iteration}.npz'))
 
 def init_inception():
     block_idx = InceptionV3.BLOCK_INDEX_BY_DIM[DIMS]
     model = InceptionV3([block_idx])
     if CUDA:
         model.cuda()
+    model.eval()
     return model.cuda()
 
 def save_statistics(model, images_dir, save_path):
@@ -43,14 +41,29 @@ def save_statistics(model, images_dir, save_path):
 
 def save_fid_scores(ref_stats_path, model_dir):
     fid_scores = {}
-    for run in os.listdir(model_dir):
-        test_stat_dir = join(model_dir, run, 'bench')
-        for path in os.listdir(test_stat_dir):
+    for run in loop_path(model_dir):
+        test_stat_dir = join(run, 'bench')
+        for path in loop_path(test_stat_dir):
             if 'npz' not in path:
                 continue
-            paths = (ref_stats_path, join(test_stat_dir, path))
+            paths = (ref_stats_path, path)
             _, _, fid_score = calculate_fid_given_paths(paths, BATCH_SIZE, CUDA, DIMS)
             fid_scores[path.split('.')[0]] = fid_score
         with open(join(test_stat_dir, 'fid.json'), 'w') as f:
             json.dump(fid_scores, f, indent=2)
+
+def save_inception_scores(model_dir):
+    inception_scores = {}
+    for run in loop_path(model_dir):
+        test_images_dir = join(run, 'images')
+        for iteration in loop_path(test_images_dir):
+            mu, sigma = inception_score(iteration, resize=True)
+            inception_scores[iteration.split('/')[-1]] = {'mu' : mu,
+                                                          'sigma' : sigma}
+        with open(join(run, 'bench', 'inception.json'), 'w') as f:
+            json.dump(inception_scores, f)
+
+def loop_path(path):
+    files = os.listdir(path)
+    return [join(path, f) for f in files]
 
