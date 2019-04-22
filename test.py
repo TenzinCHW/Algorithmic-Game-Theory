a7e158226248b2d85db60444bb847772d75fc9b8
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-04-22 16:53:34 +0800

    Refactored some things out for easier control over which models to test

--- test.py
+++ test.py
@@ -2,17 +2,22 @@ import os
 from os.path import join, isfile, expanduser
 from benchmark.scoring import *
 
-ref_stat_dir = 'results/ref'
-if not isfile(join(ref_stat_dir, 'ref_fid.npz')):
-    save_ref_statistics(expanduser('~/Datasets/cifar-10/images'), ref_stat_dir)
-
-for model_type in os.listdir('results'):
-    if model_type == 'ref':
-        continue
+def test_model_type(model_type):
     model_dir = join('results', model_type)
     save_test_statistics(model_dir)
 
     ref_stat_path = join(ref_stat_dir, 'ref_fid.npz')
     save_fid_scores(ref_stat_path, model_dir)
+
     save_inception_scores(model_dir)
+
+if __name__ == '__main__':
+    ref_stat_dir = 'results/ref'
+    if not isfile(join(ref_stat_dir, 'ref_fid.npz')):
+        save_ref_statistics(expanduser('~/Datasets/cifar-10/images'), ref_stat_dir)
+
+    for model_type in os.listdir('results'):
+        if model_type == 'ref':
+            continue
+        test_model_type(model_type)
 
