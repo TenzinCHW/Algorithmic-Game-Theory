b08f8903a9a80e184310886ae4a9a6023a152f89
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-06-01 17:41:14 +0800

    Updated main file, probably to only run single model

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from Trainer import Trainer
-from models import wgan, wgan_gp, DCGAN, SAGAN
+from models import DCGAN, DCGAN_2N, DCWGAN_2N, SAGAN
 from model_utils import opt
 from model_utils.dataloaders import train_loader
 from model_utils.torchnn import cudnn
@@ -7,10 +7,10 @@ from model_utils.torchnn import cudnn
 cudnn.benchmark = True
 models = [wgan, wgan_gp, DCGAN]
 
-trainer = Trainer(DCGAN, train_loader, opt)
+trainer = Trainer(DCWGAN_2N, train_loader, opt)
 trainer.train_hyperparams()
 
-for model in models:
-    trainer = Trainer(model, train_loader, opt)
-    trainer.train_hyperparams()
-
+#for model in models:
+#    trainer = Trainer(model, train_loader, opt)
+#    trainer.train_hyperparams()
+#
